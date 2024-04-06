5213b5e81a0c24fcb4e7a5a4cd6901266a862d59
Estrid4Ever <johannes-sr@hotmail.com>
2024-04-06 18:42:38 +0200

    github publish
    github publish

---
+++ src/main/java/se/jostra/terraquizr/TerraQuizrApplication.java
@@ -0,0 +1,13 @@
+package se.jostra.terraquizr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerraQuizrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TerraQuizrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/jostra/terraquizr/TerraQuizrApplicationTests.java
@@ -0,0 +1,13 @@
+package se.jostra.terraquizr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerraQuizrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
